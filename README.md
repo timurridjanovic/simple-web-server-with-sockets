20b838492929dc2cf1c0da14c12e7d3e7925c6be
timurridjanovic <timur.ridjanovic@gmail.com>
2014-09-19 23:23:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Simple python web server using sockets: 
+
+**Description**:  
+It's a simple python web server using sockets. It serves static css and javascript files and has custom templating.
+
+Just execute the web_server_sockets.py and see the result on localhost:8080
---
+++ web_server_sockets.py
@@ -0,0 +1,113 @@
+
+import socket
+
+def start_server():
+    host = ''
+    port = 8080
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((host, port))
+    sock.listen(1)
+
+    request_dict = {
+        "request": None,
+        "path": None
+    }
+    
+    print "server listening in on localhost:" + str(port)
+
+    while True:
+        csock, caddr = sock.accept()
+        req = csock.recv(1024)
+        request = req.split("\n")
+        request_first_line = request[0]
+        fields = request[-1]
+        request_dict["request"] = request_first_line.split(" ")[0]
+        request_dict["path"] = request_first_line.split(" ")[1]
+        if fields:
+            fields_dict = {}
+            for field in fields.split("&"):
+                (key, value) = field.split("=")
+                fields_dict[key] = value
+            request_dict["fields"] = fields_dict 
+        (body, response) = app(request_dict)
+        response = "HTTP/1.0 200 OK \nContent-Type: " + response + "\n\n"
+        csock.sendall(response + "\n".join(body))
+        csock.close()
+
+def app(environ):
+    if environ["request"] == "GET":
+        return handle_get_request(environ)
+    elif environ["request"] == "POST":
+        return handle_post_request(environ)
+
+def handle_get_request(environ):
+    path_vars = [e for e in environ["path"].split("/") if e]
+
+    if environ["path"] in router:
+        return (router[environ["path"]].get(), "text/html")
+
+    elif path_vars[0] == "css" and len(path_vars) > 1:
+        return (serve_css(environ["path"]), "text/css")
+
+    elif path_vars[0] == "js" and len(path_vars) > 1:
+        return (serve_javascript(environ["path"]), "application/javascript")
+
+    else:
+        return (["404 Error"], "text/html")
+
+def handle_post_request(environ):
+    if environ["path"] in router:
+        return (router[environ["path"]].post(environ["fields"]), "text/html")
+    else:
+        return (["404 Error"], "text/html")
+
+def serve_page(page, template_vars=None):
+    with open("./views/" + page + ".html", "r") as p:
+        template = p.readlines()
+        if template_vars == None:
+            return template
+        for e in template_vars.keys():
+            template = [line.replace("{{" + e + "}}", template_vars[e]) for line in template]
+        return template
+
+def serve_css(path):
+    try:
+        with open("./" + path, "r") as p:
+            return p.readlines()
+    except IOError:
+        return ["no such file"]
+
+def serve_javascript(path):
+    try:
+        with open("./" + path, "r") as p:
+            return p.readlines()
+    except IOError:
+        return ["no such file"]
+
+class Index(object):
+    @classmethod
+    def get(self):
+        return serve_page("index", {"name": "Timur", "phone": "123-4567"})
+
+    @classmethod
+    def post(self, fields):
+        return serve_page("index", fields)
+        
+class Hello(object):
+    @classmethod
+    def get(self):
+        return serve_page("hello")
+
+    @classmethod
+    def post(self, fields):
+        return serve_page("hello", fields)
+
+
+router = {
+    "/": Index,
+    "/hello": Hello
+}
+
+if __name__ == "__main__":
+    start_server()
